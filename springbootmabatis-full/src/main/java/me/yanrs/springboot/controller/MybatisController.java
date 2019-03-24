1b3e9802fc61a997c5d2ea88c0bef105f701f1dd
rexyan <1572402228@qq.com>
2019-03-24 18:40:16 +0800

    Add files via upload

---
+++ springbootmabatis-full/src/main/java/me/yanrs/springboot/controller/MybatisController.java
@@ -0,0 +1,23 @@
+package me.yanrs.springboot.controller;
+
+import me.yanrs.springboot.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MybatisController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/boot/students")
+    public Object student(){
+        return studentService.getAllStudent();
+    }
+
+    @GetMapping("/boot/update")
+    public int update_student(){
+        return studentService.update();
+    }
+}
